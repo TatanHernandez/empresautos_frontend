5c78a5298bb0ee46e022169ae4e9f205f877db9f
TatanHernandez <minijonathancr7@gmail.com>
2022-11-15 23:43:20 -0500

    frontend empresautos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClienteComponent } from './component/clientes/cliente.component';
+import { CochesComponent } from './component/coches/coches.component';
+import { MarcaComponent } from './component/marca/marca.component';
+import { VentasComponent } from './component/ventas/ventas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'clientes',
+    component: ClienteComponent
+  },
+
+  {
+    path: 'coches',
+    component: CochesComponent
+  },
+
+  {
+    path: 'marca',
+    component: MarcaComponent
+  },
+
+  {
+    path: 'ventas',
+    component: VentasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { MainComponent } from './layout/main/main.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ClienteComponent } from './component/clientes/cliente.component';
+import { CochesComponent } from './component/coches/coches.component';
+import { MarcaComponent } from './component/marca/marca.component';
+import { VentasComponent } from './component/ventas/ventas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    ClienteComponent,
+    CochesComponent,
+    MarcaComponent,
+    VentasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
